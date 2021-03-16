d37f02585c618ef0e4c9a5cd16c4d6e7b03c5fb4
Jayesh Patil <Jayesh_Patil@epam.com>
2021-03-16 17:34:01 +0530

    Add text for recommended movies

--- src/components/RecommendedMovies/RecommendedMovies.js
+++ src/components/RecommendedMovies/RecommendedMovies.js
@@ -5,9 +5,7 @@ import MovieCard from "../MovieCard/MovieCard";
 const RecommendedMovies = (props) => {
   return (
     <Fragment>
-      <div style={{ padding: "1rem" }}>
-        Films by {props.movieDetails.genres[0]} genre
-      </div>
+      <div style={{ padding: "1rem" }}>Recommended movies</div>
       <Row xs={1} sm={2} md={3} lg={4}>
         {props.moviesByGenre && props.moviesByGenre.length > 0 ? (
           props.moviesByGenre.map((movie) => (
