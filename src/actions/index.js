33acd8a1a5f4ce12d7694cb167d80294b94a2d03
Jayesh Patil <Jayesh_Patil@epam.com>
2021-03-24 13:05:59 +0530

    Fetch data from real api

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,61 +6,100 @@ import {
   FETCH_MOVIE_DETAILS,
   FETCH_MOVIES_BY_GENRE,
 } from "./types";
-import { moviesDataSortedByRatings } from "../api/ratingsapi";
-import { mockData } from "../api/mock";
-import { searchDataByTitle } from "../api/searchDataByTitle";
-import { searchDataByGenre } from "../api/searchDataByGenre";
-import { movieDetails } from "../api/movieDetails";
-import { moviesByGenre } from "../api/moviesByGenre";
+// import { moviesDataSortedByRatings } from "../api/ratingsapi";
+// import { mockData } from "../api/mock";
+// import { searchDataByTitle } from "../api/searchDataByTitle";
+// import { searchDataByGenre } from "../api/searchDataByGenre";
 
 const api = `https://reactjs-cdp.herokuapp.com/movies`;
 
-export const upcomingMovies = () => (dispatch) => {
-  dispatch({
-    type: FETCH_UPCOMING_MOVIES,
-    payload: mockData.data,
-  });
+export const upcomingMovies = () => async (dispatch) => {
+  try {
+    const response = await fetch(
+      `${api}?limit=20&sortBy=release_date&sortOrder=desc`
+    );
+    const movies = await response.json();
+    dispatch({
+      type: FETCH_UPCOMING_MOVIES,
+      payload: movies.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const topRatedMovies = () => (dispatch) => {
-  dispatch({
-    type: FETCH_TOP_RATED_MOVIES,
-    payload: moviesDataSortedByRatings.data,
-  });
+export const topRatedMovies = () => async (dispatch) => {
+  try {
+    const response = await fetch(
+      `${api}?sortBy=vote_average&sortOrder=desc&limit=20`
+    );
+    const movies = await response.json();
+    dispatch({
+      type: FETCH_TOP_RATED_MOVIES,
+      payload: movies.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const searchMoviesByTitle = (query) => (dispatch) => {
-  dispatch({
-    type: SEARCH_MOVIES_BY_TITLE,
-    payload: searchDataByTitle.data,
-  });
+export const searchMoviesByTitle = (query) => async (dispatch) => {
+  try {
+    const response = await fetch(
+      `${api}/?search=${query}&searchBy=title&limit=20`
+    );
+    const movies = await response.json();
+    dispatch({
+      type: SEARCH_MOVIES_BY_TITLE,
+      payload: movies.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const searchMoviesByGenre = (query) => (dispatch) => {
-  dispatch({
-    type: SEARCH_MOVIES_BY_GENRE,
-    payload: searchDataByGenre.data,
-  });
+export const searchMoviesByGenre = (query) => async (dispatch) => {
+  try {
+    const response = await fetch(
+      `${api}/?search=${query}&searchBy=genres&limit=20`
+    );
+    const movies = await response.json();
+    dispatch({
+      type: SEARCH_MOVIES_BY_GENRE,
+      payload: movies.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const getMovieDetails = (movieId) => async (dispatch) => {
-  const response = await fetch(`${api}/${movieId}`);
-  const movie = await response.json();
-
-  dispatch({
-    type: FETCH_MOVIE_DETAILS,
-    movieDetails: movie,
-  });
+  try {
+    const response = await fetch(`${api}/${movieId}`);
+    const movie = await response.json();
+    dispatch({
+      type: FETCH_MOVIE_DETAILS,
+      movieDetails: movie,
+    });
+  } catch (err) {
+    //TODO: add action to handle api request failures
+    console.log(err);
+  }
 };
 
 export const getMoviesByGenre = (genre) => async (dispatch) => {
-  const response = await fetch(
-    `${api}/?search=${genre}&searchBy=genres&limit=10`
-  );
-  const movies = await response.json();
-  console.log(movies);
-  dispatch({
-    type: FETCH_MOVIES_BY_GENRE,
-    moviesByGenre: movies.data,
-  });
+  try {
+    console.log("getMoviesByGenre");
+    const response = await fetch(
+      `${api}/?search=${genre}&searchBy=genres&limit=10`
+    );
+    const movies = await response.json();
+    dispatch({
+      type: FETCH_MOVIES_BY_GENRE,
+      moviesByGenre: movies.data,
+    });
+  } catch (err) {
+    //TODO: add action to handle api request failures
+    console.log(err);
+  }
 };
